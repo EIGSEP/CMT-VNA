41b8f625e46ab7ef12ea8d70182847d320c7727d
Latte Panda <chbye@berkeley.edu>
2025-01-24 19:56:20 -0800

    new data

--- scripts/measure_s11.py
+++ scripts/measure_s11.py
@@ -9,7 +9,7 @@ parser = ArgumentParser(
     formatter_class=ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
-    "--cal", action="store_true", default=False, help="Perform calibration."
+    "--cal", default=False, action='store_true', help="Perform calibration."
 )
 parser.add_argument(
     "--fstart", type=float, default=1e6, help="Start frequency in Hz."
@@ -27,13 +27,13 @@ parser.add_argument(
     "--power", type=float, default=-40, help="Power level in dBm."
 )
 parser.add_argument(
-    "--cadence",
+    "-c", "--cadence",
     type=float,
     default=300,
     help="How often to measure S11 in seconds.",
 )
 parser.add_argument(
-    "--max_files",
+    "-m", "--max_files",
     type=int,
     default=100,
     help="Maximum number of files to save.",
@@ -47,13 +47,7 @@ args = parser.parse_args()
 vna = VNA(ip="127.0.0.1", port=5025)
 print(f"Connected to {vna.id}.")
 
-if args.cal:
-    vna.calibrate_OSL()
-    print("Calibration complete.")
-    print("Connect DUT and hit enter")
-    input()
-
-vna.setup_S11(
+freq = vna.setup(
     fstart=args.fstart,
     fstop=args.fstop,
     npoints=args.npoints,
@@ -61,12 +55,24 @@ vna.setup_S11(
     power_dBm=args.power,
 )
 
+date = datetime.now().strftime("%Y%m%d_%H%M%S")
+np.savez(f"{args.outdir}/freqs/{date}_freqs.npz", freq=freq)
+
+if args.cal:
+    OSL = vna.calibrate_OSL()
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    #save calibration data
+    np.savez(f"{args.outdir}/cals/{date}_calibration.npz", open=OSL['open'], short=OSL['short'], load=OSL['load'])
+    print("Calibration complete.")
+    print("Connect DUT and hit enter")
+    input()
+
 i = 0
 while i < args.max_files:
     try:
-        freq, s11 = vna.measure_S11()
+        s11 = vna.measure_S11(verbose=True)
         date = datetime.now().strftime("%Y%m%d_%H%M%S")
-        np.savez(f"{args.outdir}/{date}.npz", freq=freq, s11=s11)
+        np.savez(f"{args.outdir}/{date}.npz", s11=s11)
         i += 1
         time.sleep(args.cadence)
     except KeyboardInterrupt:
