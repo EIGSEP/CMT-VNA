cd35b017a0ad2a89979fde01210e3383be1c6654
Latte Panda <chbye@berkeley.edu>
2025-01-25 16:47:46 -0800

    blanket update

--- cmt_vna/__init__.py
+++ cmt_vna/__init__.py
@@ -2,3 +2,4 @@ __author__ = "Christian Hellum Bye"
 __version__ = "0.0.1"
 
 from cmt_vna.vna import VNA
+from cmt_vna.calkit import S911T
--- scripts/plot_new.py
+++ scripts/plot_new.py
@@ -8,24 +8,41 @@ from datetime import datetime
 import warnings
 warnings.filterwarnings('ignore')
 
-def load_npz_data(filename):
+def calibrate(cal_file, s11):
+    """
+    Calibrate the s11 data we're plotting.
+
+    Parameters:
+        cal_file (str): Path to npz file, must contain keyword arguments "open", "short", "load"
+        s11 (np.array): Array containing s11 data. Must have same number of points as data in cal_file.
+
+    Returns:
+        np.array: Calibrated data.
+    """
+    #TODO: Write this function lol. Need to get Christian's branch of mistdata in the stack.
+
+def load_npz_data(filename, cal_file=None):
     """
     Load data from a .npz file.
 
     Parameters:
         filename (str): Path to the .npz file to load.
+        cal_file (None or str): Points to the calibration file to be used.
 
     Returns:
         numpy.lib.npyio.NpzFile: The loaded data object, or None if loading fails.
     """
     try:
         data = np.load(filename)['s11']
+        if cal_file:
+            #TODO: write calibration function in this script with mistdata functions
+             return None
         return data
-    except Exception as e:
-        print(f"Error loading {filename}: {e}")
+    except:
+        print('Unable to load file. Returning None')
         return None
 
-def live_plot(freq, s11_file, live_dir, output_filename):
+def live_plot(freq, s11_file, live_dir, output_filename, cal_file=None):
     """
     Continuously check a directory for new .npz files and plot new data on top of the existing plot.
 
@@ -34,8 +51,12 @@ def live_plot(freq, s11_file, live_dir, output_filename):
         s11_data (str): Initial S11 file.
         live_dir (str): Directory to watch for new .npz files.
         output_filename (str): File to which the plot figure will be saved.
+        cal_file (None or str): If none, it will plot raw S11.
     """
-    s11_data = load_npz_data(s11_file)
+    if cal_file:
+        s11_data = load_npz_data(s11_file, cal_file=cal_file)
+    else:
+        s11_data = load_npz_data(s11_file)
     plt.ion()  # Enable interactive mode
     fig, ax = plt.subplots(figsize=(10, 6))
     s11_line, = ax.plot(freq, 20*np.log10(np.abs(s11_data)), label=f"S11 ({s11_file[-19:-4]})", color="tab:red")
@@ -49,12 +70,15 @@ def live_plot(freq, s11_file, live_dir, output_filename):
     time_to_beat = s11_file
     try:
         while True:
-            new_files = [os.path.join(live_dir,i) for i in os.listdir(live_dir) if (os.path.join(live_dir, i) > time_to_beat) & ('freq' not in i)]
+            new_files = [os.path.join(live_dir,i) for i in os.listdir(live_dir) if (os.path.join(live_dir, i) > time_to_beat) & ('npz' in i)]
             if len(new_files) > 0:
                 time_to_beat = new_files[-1]
                 time.sleep(1)
                 for i in new_files:
-                    s11_new = load_npz_data(i) 
+                    if cal_file:
+                        s11_data = load_npz_data(i, cal_file=cal_file)
+                    else:
+                        s11_new = load_npz_data(i) 
                     ax.plot(freq, 20*np.log10(np.abs(s11_new)), label=f"S11 ({i[-19:-4]})", alpha=0.7)
                     plt.draw()
                     plt.pause(0.1)
@@ -67,29 +91,24 @@ def main():
     Main function to parse arguments, load data, and plot the results.
     """
     parser = argparse.ArgumentParser(description="Load and plot data from .npz files.")
-    parser.add_argument("freq", help="Path to the frequency .npz file.")
     parser.add_argument("s11", help="Path to the S11 .npz file.")
     parser.add_argument("--output", default=None, help="Filename to save the figure (optional).")
     parser.add_argument("--live", default=None, help="Directory to watch for live S11 data.")
-
+    parser.add_argument("--cal", default=False, action="store_true", help="If True, will automatically calibrate s11 data before plotting. Default is False.")
     args = parser.parse_args()
 
     # Load data from the provided files
-    freq_data = np.load(args.freq)
-    s11_data = load_npz_data(args.s11)
-
-    if freq_data is None or s11_data is None:
-        print("Failed to load one or more files. Exiting.")
-        return
-
     try:
-        freq = freq_data["freq"]
-        s11 = s11_data
-    except KeyError as e:
-        print(f"Missing expected data in one of the files: {e}")
+        freq_file = max(os.listdir(f'{args.live}/freqs'))
+    except:
+        print('no frequency file to pull from. exiting')
         return
+    freq = np.load(os.path.join(f'{args.live}/freqs', freq_file))['freq']
 
-    live_plot(freq, args.s11, args.live, args.output)
+    if args.cal:
+        live_plot(freq, args.s11, args.live, args.output, max(os.listdir(f'{args.live}/cals')))
+    else:
+        live_plot(freq, args.s11, args.live, args.output)
 
 if __name__ == "__main__":
     main()
