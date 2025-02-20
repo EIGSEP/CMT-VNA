4f28d8500f6443f65734dab7e0714382b8bba850
Latte Panda <chbye@berkeley.edu>
2025-02-20 15:15:27 -0800

    adding new flag options to calibrate and vna

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -8,16 +8,17 @@ parser = ArgumentParser(
     formatter_class = ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("--outdir", type=str, default='/home/charlie/eigsep/CMT-VNA/data', help='Output directory')
-
+parser.add_argument("--points", type=int, default=1001, help='npoints to collect')
+parser.add_argument("--ifbw", type=int, default=100, help='if bandwidth [Hz]')
 args = parser.parse_args()
 
-vna = VNA(ip='127.0.0.1', port=5025)
+vna = VNA(to=1000000, ip='127.0.0.1', port=5025)
 freq = vna.setup(
-    fstart = 1e6,
+    fstart = 50e6,
     fstop = 250e6,
-    npoints = 1001,
-    ifbw = 100,
-    power_dBm = -40)
+    npoints = args.points,
+    ifbw = args.ifbw,
+    power_dBm = 0)
 
 date = datetime.now().strftime("%Y%m%d_%H%M%S")
 np.savez(f'{args.outdir}/{date}_freqs.npz', freq=freq)
