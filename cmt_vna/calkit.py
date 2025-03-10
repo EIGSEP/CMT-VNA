47ea82f454e7a81d41a3d6fe47ea2c76c6928711
Latte Panda <chbye@berkeley.edu>
2025-03-10 12:06:25 -0700

    vna dev

--- cmt_vna/calkit.py
+++ cmt_vna/calkit.py
@@ -52,10 +52,8 @@ class S911T(CalKit):
         gamma = np.vstack([open_gamma, shor_gamma, load_gamma])
         return gamma
 
-    def calibrate(self, cal_file, s11):
-        gamma_true = self.std_gamma
+    def get_sparams(model_stds, cal_file):
         osl = np.load(cal_file)
         gamma_measured = np.vstack([osl['open'], osl['short'], osl['load']])
-        sparams = cal_s11.network_sparams(gamma_true, gamma_measured)
-        gamma_prime = cal_s11.embed_sparams(sparams, s11)
+        sparams = cal_s11.network_sparams(model_stds, gamma_measured)
         return gamma_prime
