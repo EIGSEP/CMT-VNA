e4a0cee3d3c3346fc3383b1f852693a64fd16606
cgtolley <charlieglenn.t@gmail.com>
2025-04-03 11:34:25 -0700

    re oved dependance on mist.

--- cmt_vna/__init__.py
+++ cmt_vna/__init__.py
@@ -2,4 +2,4 @@ __author__ = "Christian Hellum Bye, Charlie Tolley"
 __version__ = "0.0.1"
 
 from cmt_vna.vna import VNA
-from cmt_vna.calkit import S911T
+from cmt_vna import calkit
--- tests/test_vna.py
+++ tests/test_vna.py
@@ -1,21 +1,4 @@
 import numpy as np
 from cmt_vna import VNA
 from cmt_vna import S911T
-from mistdata import cal_s11 as cal
 
-DATA_DIR = '../data/'
-
-CAL_DIR = os.path.join(DATA_DIR, 'cals')
-CAL_FILE = os.path.join(CAL_DIR, max(os.listdir(CAL_DIR)))
-CAL = np.load(CAL_FILE)
-osl = np.vstack([CAL['open'], CAL['short'], CAL['load']])
-FREQS = CAL['freqs']
-calkit = S911T(freq_Hz=FREQS) #new calkit with meas freqs
-
-def test_vna_sprms(calkit, osl, CAL_FILE):
-    vna_sprms = cal.network_sparams(gamma_true=calkit.std_gamma, gamma_meas=osl) #vna_prms acquired
-    
-    vna = VNA()
-    vna.add_sparams(stds_file=CAL_FILE, kit=calkit)
-    
-    assert np.allclose(vna.sparams, vna_sprms)
