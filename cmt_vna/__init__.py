0e795f953b2567e9817b2f86dc4381c82111223c
Christian Hellum Bye <chbye@berkeley.edu>
2025-04-04 11:14:26 -0700

    try/except for functionality connecting to VNA

--- cmt_vna/__init__.py
+++ cmt_vna/__init__.py
@@ -1,5 +1,11 @@
 __author__ = "Christian Hellum Bye, Charlie Tolley"
 __version__ = "0.0.1"
 
-from cmt_vna.vna import VNA
+try:
+    from cmt_vna.vna import VNA
+except ImportError:
+    print(
+        "Not importing VNA, as it requires pyvisa and pyvisa-py. Can't connect"
+        "to VNA, but can still calibrate and use the calibration kit."
+    )
 from cmt_vna import calkit
