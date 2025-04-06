d80896324aab0a50d7bc18f48e6ba79f7b687bca
cgtolley <charlieglenn.t@gmail.com>
2025-04-06 13:53:17 -0700

    working on plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,8 +7,16 @@ parser = ArgumentParser(description='Calibrate and plot S11 data.',
     formatter_class = ArgumentDefaultsHelpFormatter
 )
 
-parser.add_argument('--file', '-f', help='file data is stored in')
+parser.add_argument('--file', '-f', help='file prefix.')
 
 parser.add_argument('--outdir', default='/home/charlie/eigsep/CMT-VNA/data/', help='where outputs will be saved')
 
-args = 
+args = parser.parse_args()
+
+vna = VNA()
+
+sparams = np.load(f'{args.file}_sparams.npz').values()
+standards = np.load(f'{args.file}standards.npz').values()
+gammas = np.load(f'{args.file}_gammas.npz').values()
+
+
