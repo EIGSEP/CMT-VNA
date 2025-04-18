6258594df4f1485a2538a42a8c80e2085c0782a2
Christian Hellum Bye <chbye@berkeley.edu>
2025-04-17 18:01:19 -0700

    require pyvisa and pyvisapy

--- cmt_vna/__init__.py
+++ cmt_vna/__init__.py
@@ -1,11 +1,5 @@
 __author__ = "Christian Hellum Bye, Charlie Tolley"
 __version__ = "0.0.1"
 
-try:
-    from cmt_vna.vna import VNA
-except ImportError:
-    print(
-        "Not importing VNA, as it requires pyvisa and pyvisa-py. Can't connect"
-        "to VNA, but can still calibrate and use the calibration kit."
-    )
 from cmt_vna import calkit
+from cmt_vna.vna import VNA
