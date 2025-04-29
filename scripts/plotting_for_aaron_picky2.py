b5d73458db9200188c628814a92573c8365b2e07
Charlie Tolley <chbye@berkeley.edu>
2025-04-29 12:03:19 -0700

    not totally sure what's happening

--- scripts/plotting_for_aaron_picky2.py
+++ scripts/plotting_for_aaron_picky2.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot as plt
 from cmt_vna import VNA
 
 vna = VNA()
-freqs = vna.setup(fstart=250e6, fstop=1.8e9, power_dBm=0)
-data = vna.measure_S11()
+freqs = vna.setup(fstart=250e6, fstop=1.8e9, npoints=1000, power_dBm=0)
+#input('please connect the jank board and press enter.')
+jank_data = vna.measure_S11()
+#print(len(jank_data))
+#input('please connect the rocketship board and press enter.')
+#rocketship_data = vna.measure_S11()
+#print(len(rocketship_data))
 
 plt.figure()
-plt.plot(freqs/1e6, 20*np.log10(np.abs(data)))
+plt.plot(freqs/1e6, 20*np.log10(np.abs(jank_data)), label='jank board')
+#plt.plot(freqs/1e6, 20*np.log10(np.abs(rocketship_data)), label='rocketship board')
+plt.legend()
 plt.show()
