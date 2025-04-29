269c662a17d62e077319a51a099367d744109683
Charlie Tolley <chbye@berkeley.edu>
2025-04-29 12:10:42 -0700

    new data

--- scripts/plotting_for_aaron_picky2.py
+++ scripts/plotting_for_aaron_picky2.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from cmt_vna import VNA
 
 vna = VNA()
-freqs = vna.setup(fstart=250e6, fstop=1.8e9, npoints=1000, power_dBm=0)
-#input('please connect the jank board and press enter.')
+freqs = vna.setup(fstart=50e6, fstop=250e6, npoints=1000, power_dBm=0)
+input('please connect the jank board and press enter.')
 jank_data = vna.measure_S11()
-#print(len(jank_data))
-#input('please connect the rocketship board and press enter.')
-#rocketship_data = vna.measure_S11()
-#print(len(rocketship_data))
+print(len(jank_data))
+input('please connect the rocketship board and press enter.')
+rocketship_data = vna.measure_S11()
+print(len(rocketship_data))
 
 plt.figure()
 plt.plot(freqs/1e6, 20*np.log10(np.abs(jank_data)), label='jank board')
-#plt.plot(freqs/1e6, 20*np.log10(np.abs(rocketship_data)), label='rocketship board')
+plt.plot(freqs/1e6, 20*np.log10(np.abs(rocketship_data)), label='rocketship board')
 plt.legend()
 plt.show()
