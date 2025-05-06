e01699d3fc6ab13effa9d21cf9597187757b958c
Charlie Tolley <chbye@berkeley.edu>
2025-05-06 13:41:59 -0700

    new data, updated scripts

--- scripts/take_data.py
+++ scripts/take_data.py
@@ -2,9 +2,9 @@ import numpy as np
 import sys
 from cmt_vna import VNA
 
-#FSTART, FSTOP = 250e6, 1.8e9
+FSTART, FSTOP = 250e6, 1.8e9
 #FSTART, FSTOP = 50e6, 250e6
-FSTART, FSTOP = 10e6, 1e9
+#FSTART, FSTOP = 10e6, 1e9
 
 assert len(sys.argv) == 2
 filename = sys.argv[-1]
