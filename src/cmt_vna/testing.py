8657dcea2bfd55e4b8a57a74c78dbd5341f0e01b
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-29 21:16:17 -0700

    specify exception in try/except

--- src/cmt_vna/testing.py
+++ src/cmt_vna/testing.py
@@ -95,7 +95,7 @@ class DummyVNA(VNA):
         """
         try:
             f = np.linspace(self.fstart, self.fstop, self.npoints)
-        except:
+        except AttributeError:
             f = None
         return f
 
